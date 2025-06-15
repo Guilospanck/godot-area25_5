e4df0350c9938136f9ed4bb46ca64b48803ea0f3
Guilherme Pereira <guilospanck@protonmail.com>
2025-06-15 11:50:11 +0100

    Add walls and change player sprite when moving

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,16 +2,33 @@ extends CharacterBody2D
 
 @onready var animated_player: AnimatedSprite2D = $Animations
 
+const NOT_MOVING: Vector2 = Vector2(0, 0)
+
+# TODO: add this to ammo
+# func _on_visible_on_screen_notifier_2d_screen_exited():
+#	queue_free()
+
+# TODO: a way of removing all elements belonging to a group
+# get_tree().call_group("mobs", "queue_free")
+
 var speed: int = 300
 
 func _ready() -> void:
 	animated_player.play("idle")
 
-
-func get_input():
+func process_input():
 	var input_dir = Input.get_vector("left", "right", "top", "down")
 	velocity = input_dir * speed
 
-func _physics_process(delta):
-	get_input()
-	move_and_collide(velocity * delta)
+	animated_player.flip_h = input_dir.x < 0
+
+	if input_dir == NOT_MOVING:
+		animated_player.play("idle")
+	else:
+		animated_player.play("run")
+
+	move_and_slide()
+
+func _physics_process(_delta: float):
+	process_input()
+
