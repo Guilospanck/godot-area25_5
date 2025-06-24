181e483c5e4fcbeaedd0e09cf1c77d1112f221e4
Guilherme Pereira <guilospanck@protonmail.com>
2025-06-24 18:46:42 +0100

    Update resources

--- scripts/resources/StatResource.cs
+++ scripts/resources/StatResource.cs
@@ -3,26 +3,26 @@ using System;
 
 public partial class StatResource : Resource
 {
-    [Export]
-    public String Name;
+	[Export]
+	public String Name;
 
-    [ExportCategory("Stat orientation")]
-    [Export]
-    public Vector2 Position;
-    [Export]
-    public float Rotation;
-    [Export]
-    public Vector2 Scale;
+	[ExportCategory("Stat orientation")]
+	[Export]
+	public Vector2 Position;
+	[Export]
+	public float Rotation;
+	[Export]
+	public Vector2 Scale;
 
-    [ExportCategory("Visual settings")]
-    [Export]
-    public String TexturePath;
+	[ExportCategory("Visual settings")]
+	[Export]
+	public String TexturePath;
 
-    [ExportCategory("Metadata")]
-    [Export]
-    public int Stat; // TODO: change
+	[ExportCategory("Metadata")]
+	[Export]
+	public int Stat; // TODO: change
 
-    [ExportCategory("Collision")]
-    [Export]
-    public Shape2D CollisionShape;
+	[ExportCategory("Collision")]
+	[Export]
+	public Shape2D CollisionShape;
 }
